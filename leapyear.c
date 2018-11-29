86f030f369e9cc56133988c6e54bbf2a0e6a5377
leblancbot <45463498+leblancbot@users.noreply.github.com>
2018-11-29 22:52:43 +0600

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,17 @@
+main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%400==0)
+        printf("Leap Year");
+    else if(n%100==0)
+        printf("no");
+    else if(n%4==0)
+        printf("Leap Year");
+
+    else
+
+    printf("No");
+
+
+}
